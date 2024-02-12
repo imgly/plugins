e6639f8a2b150a06bed6314c3d87fb80ec548249
Daniel Hauschildt <daniel.hauschildt@img.ly>
2024-02-12 10:07:17 +0100

    fix: removed obsolte types

--- packages/vectorizer/types/utils/constants.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const PLUGIN_ID = "@imgly/plugin-vectorizer-web";
-export declare const PLUGIN_COMPONENT_BUTTON_ID = "component.@imgly/plugin-vectorizer-web.button";
-export declare const PLUGIN_ACTION_VECTORIZE_LABEL = "plugin.@imgly/plugin-vectorizer-web.vectorize";
-export declare const PLUGIN_ICON = "@imgly/icons/Vectorize";
