90c5087444b8fd12ae8eb857f1ee61b86fe3aaf3
maerch <marcin.skirzynski@gmail.com>
2024-04-03 14:14:25 +0200

    Chores: Build System Cleanup (#10)
    * Move common esbuild code to toplevel files
    
    * Add plugin to build types during build
    
    * remote-asset-source: Use common esbuild files
    
    * background-removal: Use common esbuild files
    
    * Add .nvmrc to gitignore
    
    * Turo: Only execute `dev` since the types are now build with esbuild
    
    * Add scaffold for utils package

--- packages/remote-asset-source/scripts/watch.mjs
+++ packages/remote-asset-source/scripts/watch.mjs
@@ -1,19 +1,5 @@
-import chalk from 'chalk';
 import * as esbuild from 'esbuild';
+import config from '../esbuild/config.mjs';
 
-import configs from '../esbuild/config.mjs';
-
-console.log(
-  `[${new Date().toLocaleTimeString(undefined, {
-    hour: 'numeric',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false
-  })}] ${chalk.green('Watching...')}`
-);
-
-const contexts = await Promise.all(
-  configs.map((config) => esbuild.context(config))
-);
-
-await Promise.any(contexts.map((ctx) => ctx.watch()));
+const context = await esbuild.context(config({ isDevelopment: true }));
+await context.watch();
