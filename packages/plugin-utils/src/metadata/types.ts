a10c2f8be12311e41c8f77994d0652ed71fbbba9
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2024-04-09 17:10:52 +0200

    utils: Add metadata types

---
+++ packages/plugin-utils/src/metadata/types.ts
@@ -0,0 +1,55 @@
+import { type Source } from '@cesdk/cesdk-js';
+
+export type PluginStatusIdle = { status: 'IDLE' };
+export type PluginStatusPending = { status: 'PENDING' };
+
+export type PluginStatusProcessing = {
+  version: string;
+  status: 'PROCESSING';
+
+  initialImageFileURI: string;
+  initialSourceSet: Source[];
+  initialPreviewFileURI: string;
+
+  blockId: number;
+  fillId: number;
+
+  progress?: {
+    key: string;
+    current: number;
+    total: number;
+  };
+};
+
+export type PluginStatusProcessed = {
+  version: string;
+  status: 'PROCESSED';
+
+  initialImageFileURI: string;
+  initialSourceSet: Source[];
+  initialPreviewFileURI: string;
+
+  blockId: number;
+  fillId: number;
+
+  removedBackground: string | Source[];
+};
+
+export type PluginStatusError = {
+  version: string;
+  status: 'ERROR';
+
+  initialImageFileURI: string;
+  initialSourceSet: Source[];
+  initialPreviewFileURI: string;
+
+  blockId: number;
+  fillId: number;
+};
+
+export type PluginStatusMetadata =
+  | PluginStatusIdle
+  | PluginStatusError
+  | PluginStatusPending
+  | PluginStatusProcessing
+  | PluginStatusProcessed;
