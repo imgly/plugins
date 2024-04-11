61081bbf41bd93ceb97d214fcfbf6efb1a7a8c4a
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2024-04-11 10:33:12 +0200

    ImageProcessingMetadata: Use types from types.ts

--- packages/plugin-utils/src/metadata/ImageProcessingMetadata.ts
+++ packages/plugin-utils/src/metadata/ImageProcessingMetadata.ts
@@ -1,61 +1,12 @@
-import { type Source } from '@cesdk/cesdk-js';
 import isEqual from 'lodash/isEqual';
 
 import Metadata from './Metadata';
-
-export type PluginStatusIdle = { status: 'IDLE' };
-export type PluginStatusPending = { status: 'PENDING' };
-
-export type PluginStatusProcessing = {
-  version: string;
-  status: 'PROCESSING';
-
-  initialImageFileURI: string;
-  initialSourceSet: Source[];
-  initialPreviewFileURI: string;
-
-  blockId: number;
-  fillId: number;
-
-  progress?: {
-    key: string;
-    current: number;
-    total: number;
-  };
-};
-
-export type PluginStatusProcessed = {
-  version: string;
-  status: 'PROCESSED';
-
-  initialImageFileURI: string;
-  initialSourceSet: Source[];
-  initialPreviewFileURI: string;
-
-  blockId: number;
-  fillId: number;
-
-  removedBackground: string | Source[];
-};
-
-export type PluginStatusError = {
-  version: string;
-  status: 'ERROR';
-
-  initialImageFileURI: string;
-  initialSourceSet: Source[];
-  initialPreviewFileURI: string;
-
-  blockId: number;
-  fillId: number;
-};
-
-export type PluginStatusMetadata =
-  | PluginStatusIdle
-  | PluginStatusError
-  | PluginStatusPending
-  | PluginStatusProcessing
-  | PluginStatusProcessed;
+import {
+  PluginStatusError,
+  PluginStatusMetadata,
+  PluginStatusProcessed,
+  PluginStatusProcessing
+} from './types';
 
 class ImageProcessingMetadata extends Metadata<PluginStatusMetadata> {
   get(blockId: number): PluginStatusMetadata {
