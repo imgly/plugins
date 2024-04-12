ad4a9196546d14c2e135a0fca7f4c71caaf4d2ed
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2024-04-12 09:35:19 +0200

    utils: Add fillProcessing method

--- packages/plugin-utils/src/index.ts
+++ packages/plugin-utils/src/index.ts
@@ -1,3 +1,5 @@
 export { default as Metadata } from './metadata/Metadata';
 
 export { default as ImageProcessingMetadata } from './metadata/ImageProcessingMetadata';
+
+export { default as processFill } from './processing/processFill';
