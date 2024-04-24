3a8d8510f1da318067bc1b079cda5049b974eda0
Mirko Budszuhn <mirko314@users.noreply.github.com>
2024-04-24 10:22:13 +0200

    Use `credentialless` instead of require-corp for example.

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'credentialless'
+    }
+  }
 });
