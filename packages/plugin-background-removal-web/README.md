851ebdee30b25253de723c1aef80f0ee7bd16961
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2024-05-13 12:17:46 +0200

    BG-removal: Allow custom BG Removal provider as configuration

--- packages/plugin-background-removal-web/README.md
+++ packages/plugin-background-removal-web/README.md
@@ -23,28 +23,29 @@ import CreativeEditorSDK from '@cesdk/cesdk-js';
 import BackgroundRemovalPlugin from '@imgly/plugin-background-removal-web';
 
 const config = {
-  license: '<your-license-here>',
-  callbacks: {
-    // Please note that the background removal plugin depends on an correctly
-    // configured upload. 'local' will work for local testing, but in
-    // production you will need something stable. Please take a look at:
-    // https://img.ly/docs/cesdk/ui/guides/upload-images/
-    onUpload: 'local'
-  }
+    license: '<your-license-here>',
+    callbacks: {
+        // Please note that the background removal plugin depends on an correctly
+        // configured upload. 'local' will work for local testing, but in
+        // production you will need something stable. Please take a look at:
+        // https://img.ly/docs/cesdk/ui/guides/upload-images/
+        onUpload: 'local'
+    }
 };
 
 const cesdk = await CreativeEditorSDK.create(container, config);
 await cesdk.addDefaultAssetSources(),
-  await cesdk.addDemoAssetSources({ sceneMode: 'Design' }),
-  await cesdk.unstable_addPlugin(BackgroundRemovalPlugin());
+    await cesdk.addDemoAssetSources({ sceneMode: 'Design' }),
+    await cesdk.unstable_addPlugin(BackgroundRemovalPlugin());
 
 await cesdk.createDesignScene();
 ```
 
 ## Configuration
 
-All configuration options from the underlying background removal library
-can be used in this plugin.
+By default, this plugin uses the `@imgly/background-removal-js` library to remove
+a background from the image fill. All configuration options from this underlying
+library can be used in this plugin.
 
 [See the documentation](https://github.com/imgly/background-removal-js/tree/main/packages/web#advanced-configuration) for further information.
 
@@ -54,10 +55,13 @@ import BackgroundRemovalPlugin from '@imgly/plugin-background-removal-web';
 [...]
 
 await cesdk.unstable_addPlugin(BackgroundRemovalPlugin({
-  backgroundRemoval: {
-    publicPath: '...',
-    // All other configuration option that are passed to the bg removal
-    // library. See https://github.com/imgly/background-removal-js/tree/main/packages/web#advanced-configuration
+  provider: {
+    type: '@imgly/background-removal',
+    configuration: {
+        publicPath: '...',
+        // All other configuration option that are passed to the bg removal
+        // library. See https://github.com/imgly/background-removal-js/tree/main/packages/web#advanced-configuration
+    }
   }
 }))
 
@@ -70,4 +74,39 @@ For optimal performance using the correct CORS headers is important. See the lib
 ```
 'Cross-Origin-Opener-Policy': 'same-origin',
 'Cross-Origin-Embedder-Policy': 'require-corp'
+```
+
+## Custom Background Removal Provider
+
+It is possible to declare a different provider for the background removal process.
+
+```typescript
+BackgroundRemovalPlugin({
+    provider: {
+        type: 'custom',
+        // This is an optional function that can do some pre-processing, e.g. creating
+        // a mask that is applied on all images from the source set in the process function.
+        // The returned blob is passed as the second argument in `process`
+        preprocess: async (uriToProcess: string) => {
+            const mask = await segmentForeground(uriToProcess);
+            return mask;
+        },
+        process: (
+            sourceSet: {
+                uri: string;
+                width?: number;
+                height?: number;
+            }[],
+            preprocessedData?: Blob
+        ): Blob[] => {
+            const masked = await Promise.all(
+                sourceSet.map((source) => {
+                    // ...
+                })
+            );
+
+            return masked;
+        }
+    }
+});
 ```
--- packages/plugin-background-removal-web/src/processBackgroundRemoval.ts
+++ packages/plugin-background-removal-web/src/processBackgroundRemoval.ts
@@ -1,13 +1,38 @@
 import type CreativeEditorSDK from '@cesdk/cesdk-js';
+import type { Source } from '@cesdk/cesdk-js';
 import {
   applySegmentationMask,
   segmentForeground,
   type Config
 } from '@imgly/background-removal';
 
-import { processFill, type FillProcessingMetadata } from '@imgly/plugin-utils';
+import {
+  processFill,
+  type FillProcessingMetadata,
+  type Optional
+} from '@imgly/plugin-utils';
 
 import throttle from 'lodash/throttle';
+
+interface IMGLYBackgroundRemovalProviderClientSide {
+  type: '@imgly/background-removal';
+  configuration?: Config;
+}
+
+interface CustomBackgroundRemovalProvider {
+  type: 'custom';
+
+  process: (
+    sourceSet: Optional<Source, 'width' | 'height'>[],
+    preprocessedData: Blob | undefined
+  ) => Promise<Blob[]>;
+
+  preprocess?: (uri: string) => Promise<Blob>;
+}
+
+export type BackgroundRemovalProvider =
+  | IMGLYBackgroundRemovalProviderClientSide
+  | CustomBackgroundRemovalProvider;
 
 /**
  * Triggers the background removal process.
@@ -15,50 +40,72 @@ import throttle from 'lodash/throttle';
 export async function processBackgroundRemoval(
   cesdk: CreativeEditorSDK,
   blockId: number,
-  configuration: Config,
-  metadata: FillProcessingMetadata
+  metadata: FillProcessingMetadata,
+  provider: BackgroundRemovalProvider
 ) {
-  processFill<Blob>(
-    cesdk,
-    blockId,
-    metadata,
-    async (sources, mask) => {
-      const bgRemovalConfiguration = {
-        ...configuration,
-        progress: throttle((key, current, total) => {
-          const currentMetadataInProgress = metadata.get(blockId);
-          if (
-            currentMetadataInProgress.status !== 'PROCESSING' ||
-            !metadata.isConsistent(blockId)
-          )
-            return;
+  switch (provider.type) {
+    case '@imgly/background-removal': {
+      const configuration = provider.configuration ?? {};
 
-          configuration.progress?.(key, current, total);
-          metadata.set(blockId, {
-            ...currentMetadataInProgress,
-            progress: { key, current, total }
-          });
-        }, 100)
-      };
+      processFill<Blob>(
+        cesdk,
+        blockId,
+        metadata,
+        async (sources, mask) => {
+          const bgRemovalConfiguration = {
+            ...configuration,
+            progress: throttle((key, current, total) => {
+              const currentMetadataInProgress = metadata.get(blockId);
+              if (
+                currentMetadataInProgress.status !== 'PROCESSING' ||
+                !metadata.isConsistent(blockId)
+              )
+                return;
 
-      const masked = await Promise.all(
-        sources.map(async (source): Promise<Blob> => {
-          // Applying the mask to the original image
-          const blob = await applySegmentationMask(
-            source.uri,
-            mask,
-            bgRemovalConfiguration
+              configuration.progress?.(key, current, total);
+              metadata.set(blockId, {
+                ...currentMetadataInProgress,
+                progress: { key, current, total }
+              });
+            }, 100)
+          };
+
+          const masked = await Promise.all(
+            sources.map(async (source): Promise<Blob> => {
+              // Applying the mask to the original image
+              const blob = await applySegmentationMask(
+                source.uri,
+                mask,
+                bgRemovalConfiguration
+              );
+              return blob;
+            })
           );
-          return blob;
-        })
-      );
-      return masked;
-    },
+          return masked;
+        },
 
-    // Preprocessing the image by creating a segmentation mask
-    async (uriToProcess): Promise<Blob> => {
-      const mask = await segmentForeground(uriToProcess, configuration);
-      return mask;
+        // Preprocessing the image by creating a segmentation mask
+        async (uriToProcess): Promise<Blob> => {
+          const mask = await segmentForeground(uriToProcess, configuration);
+          return mask;
+        }
+      );
+      break;
     }
-  );
+
+    case 'custom': {
+      processFill<Blob>(
+        cesdk,
+        blockId,
+        metadata,
+        provider.process,
+        provider.preprocess
+      );
+      break;
+    }
+
+    default: {
+      throw new Error('Unknown background removal provider');
+    }
+  }
 }
