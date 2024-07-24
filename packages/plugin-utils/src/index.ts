deb581311700105c274f17334a5feaab9e17cf7d
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2024-07-24 09:37:40 +0200

    Add new more generic `fillProcessing` method to plugin utils

--- packages/plugin-utils/src/index.ts
+++ packages/plugin-utils/src/index.ts
@@ -2,6 +2,8 @@ export { default as Metadata } from './metadata/Metadata';
 
 export { default as FillProcessingMetadata } from './metadata/FillProcessingMetadata';
 
+export { default as fillProcessing } from './processing/fillProcessing';
+
 export { default as processFill } from './processing/processFill';
 
 export { default as initializeFillProcessing } from './processing/initializeFillProcessing';
