2862f5573eaa6edd909846ee57f692c81a13a3d1
Raven Szewczyk <git@eigenraven.me>
2025-03-06 13:01:14 +0000

    fix: vite watch issues (#46)
    * fix: Hot reload on the example repository when changing plugins
    
    * Faster dev run by waiting on files instead of forcing a full rebuild

--- examples/web/vite.config.ts
+++ examples/web/vite.config.ts
@@ -1,13 +1,42 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // https://github.com/vitejs/vite/issues/8619#issuecomment-2606507049
+    {
+      name: 'watch-workspace-packages',
+      configureServer(server) {
+        server.watcher.options = {
+          ...server.watcher.options,
+          ignored: [
+            (path) => {
+              const isNodeModules = path.includes('node_modules/');
+              const isWorkspace = Object.keys(
+                packageJson.dependenciesMeta
+              ).some(
+                (pkgName) =>
+                  path.includes(pkgName) ||
+                  path.includes(pkgName.replace('/', '+'))
+              );
+              return isNodeModules && !isWorkspace;
+            },
+            '**/.git/**'
+          ]
+        };
+      }
+    }
+  ],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'credentialless'
     }
+  },
+  optimizeDeps: {
+    exclude: Object.keys(packageJson.dependenciesMeta)
   }
 });
