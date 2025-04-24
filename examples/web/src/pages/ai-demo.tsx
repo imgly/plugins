cffceb67b8b0e51c01a3d023d71037c9b8e9332a
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2025-04-24 13:38:49 +0200

    Add gpt-image-1 model for image2image

--- examples/web/src/pages/ai-demo.tsx
+++ examples/web/src/pages/ai-demo.tsx
@@ -2,7 +2,7 @@ import CreativeEditorSDK from '@cesdk/cesdk-js';
 
 import AiApps from '@imgly/plugin-ai-apps-web';
 import OpenAiImage from '@imgly/plugin-ai-image-generation-web/open-ai';
-import FalAiImage from '@imgly/plugin-ai-image-generation-web/fal-ai';
+// import FalAiImage from '@imgly/plugin-ai-image-generation-web/fal-ai';
 import FalAiVideo from '@imgly/plugin-ai-video-generation-web/fal-ai';
 import Elevenlabs from '@imgly/plugin-ai-audio-generation-web/elevenlabs';
 import Anthropic from '@imgly/plugin-ai-text-generation-web/anthropic';
@@ -145,28 +145,28 @@ function App() {
                     ],
                     proxyUrl: import.meta.env.VITE_ANTHROPIC_PROXY_URL
                   }),
-                  text2image: OpenAiImage.GptImage1({
+                  text2image: OpenAiImage.GptImage1.Text2Image({
                     middleware: [imageRateLimitMiddleware, errorMiddleware],
                     proxyUrl: import.meta.env.VITE_OPENAI_PROXY_URL
                   }),
-                  image2image: FalAiImage.GeminiFlashEdit({
-                    middleware: [errorMiddleware, imageRateLimitMiddleware],
-                    proxyUrl: import.meta.env.VITE_FAL_AI_PROXY_URL
+                  image2image: OpenAiImage.GptImage1.Image2Image({
+                    middleware: [imageRateLimitMiddleware, errorMiddleware],
+                    proxyUrl: import.meta.env.VITE_OPENAI_PROXY_URL
                   }),
                   text2video: FalAiVideo.MinimaxVideo01Live({
-                    middleware: [errorMiddleware, videoRateLimitMiddleware],
+                    middleware: [videoRateLimitMiddleware, errorMiddleware],
                     proxyUrl: import.meta.env.VITE_FAL_AI_PROXY_URL
                   }),
                   image2video: FalAiVideo.MinimaxVideo01LiveImageToVideo({
-                    middleware: [errorMiddleware, videoRateLimitMiddleware],
+                    middleware: [videoRateLimitMiddleware, errorMiddleware],
                     proxyUrl: import.meta.env.VITE_FAL_AI_PROXY_URL
                   }),
                   text2speech: Elevenlabs.ElevenMultilingualV2({
-                    middleware: [errorMiddleware, soundRateLimitMiddleware],
+                    middleware: [soundRateLimitMiddleware, errorMiddleware],
                     proxyUrl: import.meta.env.VITE_ELEVENLABS_PROXY_URL
                   }),
                   text2sound: Elevenlabs.ElevenSoundEffects({
-                    middleware: [errorMiddleware, soundRateLimitMiddleware],
+                    middleware: [soundRateLimitMiddleware, errorMiddleware],
                     proxyUrl: import.meta.env.VITE_ELEVENLABS_PROXY_URL
                   })
                 }
