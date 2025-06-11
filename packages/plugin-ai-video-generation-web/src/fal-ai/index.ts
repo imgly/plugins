3e1bd50e14ae8585dec24b83f8dfa8025f90182f
Dogus <dogus92009@live.de>
2025-06-11 12:03:48 +0200

    Veo3TextToVideo

--- packages/plugin-ai-video-generation-web/src/fal-ai/index.ts
+++ packages/plugin-ai-video-generation-web/src/fal-ai/index.ts
@@ -3,13 +3,15 @@ import { MinimaxVideo01LiveImageToVideo } from './MinimaxVideo01LiveImageToVideo
 import { KlingVideoV21MasterImageToVideo } from './KlingVideoV21MasterImageToVideo';
 import { PixverseV35TextToVideo } from './PixverseV35TextToVideo';
 import { KlingVideoV21MasterTextToVideo } from './KlingVideoV21MasterTextToVideo';
+import { Veo3TextToVideo } from './Veo3TextToVideo';
 
 const FalAi = {
   MinimaxVideo01Live,
   MinimaxVideo01LiveImageToVideo,
   KlingVideoV21MasterImageToVideo,
   PixverseV35TextToVideo,
-  KlingVideoV21MasterTextToVideo
+  KlingVideoV21MasterTextToVideo,
+  Veo3TextToVideo
 };
 
 export default FalAi;
