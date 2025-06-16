88e97fae1d1bd1dd822b18c3657b93ec1b565ef6
Dogus <dogus92009@live.de>
2025-06-16 14:48:03 +0200

    Kling Video v2.1 (text2video, image2video)

--- packages/plugin-ai-video-generation-web/src/fal-ai/index.ts
+++ packages/plugin-ai-video-generation-web/src/fal-ai/index.ts
@@ -1,11 +1,15 @@
 import { MinimaxVideo01Live } from './MinimaxVideo01Live';
 import { MinimaxVideo01LiveImageToVideo } from './MinimaxVideo01LiveImageToVideo';
+import { KlingVideoV21MasterImageToVideo } from './KlingVideoV21MasterImageToVideo';
 import { PixverseV35TextToVideo } from './PixverseV35TextToVideo';
+import { KlingVideoV21MasterTextToVideo } from './KlingVideoV21MasterTextToVideo';
 
 const FalAi = {
   MinimaxVideo01Live,
   MinimaxVideo01LiveImageToVideo,
-  PixverseV35TextToVideo
+  KlingVideoV21MasterImageToVideo,
+  PixverseV35TextToVideo,
+  KlingVideoV21MasterTextToVideo
 };
 
 export default FalAi;
