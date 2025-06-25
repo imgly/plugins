6b7f263280714b86f8ebba5275326585c3f717e8
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2025-06-25 11:44:02 +0200

    New `initializeProvider` method with just the panel logic for now

---
+++ packages/plugin-ai-generation-web/src/generation/initializeProvider.ts
@@ -0,0 +1,62 @@
+import CreativeEditorSDK, { BuilderRenderFunction } from '@cesdk/cesdk-js';
+import Provider, { GetInput, Output, OutputKind } from './provider';
+import { InitProviderConfiguration } from './types';
+import createPanelRenderFunction from './createPanelRenderFunction';
+
+type Result<O> = { status: 'success'; output: O } | { status: 'aborted' };
+
+export type ProviderInitializationResult<
+  K extends OutputKind,
+  I,
+  O extends Output
+> = {
+  provider: Provider<K, I, O>;
+
+  panel: {
+    builderRenderFunction?: BuilderRenderFunction;
+  };
+  history?: {
+    assetSourceId: string;
+    assetLibraryEntryId: string;
+  };
+  quickActions?: {
+    registered: {
+      [kind: string]: string;
+    };
+    order: {
+      [kind: string]: string[];
+    };
+  };
+
+  // TODO: Expose generate function in the provider interface
+  generate?: (getInput: GetInput<I>) => Promise<Result<O>>;
+};
+
+async function initializeProvider<K extends OutputKind, I, O extends Output>(
+  kind: K,
+  provider: Provider<K, I, O>,
+  options: {
+    cesdk: CreativeEditorSDK;
+  },
+  config: InitProviderConfiguration
+): Promise<ProviderInitializationResult<K, I, O>> {
+  const builderRenderFunction: BuilderRenderFunction | undefined =
+    await createPanelRenderFunction({
+      provider,
+      panelInput: provider.input?.panel,
+      options: {
+        cesdk: options.cesdk,
+        engine: options.cesdk.engine
+      },
+      config
+    });
+
+  return {
+    provider,
+    panel: {
+      builderRenderFunction
+    }
+  };
+}
+
+export default initializeProvider;
