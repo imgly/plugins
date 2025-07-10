9435b146b7bc2cabeb2598cd9b1877de6adbbc89
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2025-07-10 15:52:27 +0200

    Use `kind` for creating composite source id

--- packages/plugin-ai-generation-web/src/assets/initializeHistoryCompositeAssetSource.ts
+++ packages/plugin-ai-generation-web/src/assets/initializeHistoryCompositeAssetSource.ts
@@ -1,24 +1,24 @@
 import { AggregatedAssetSource } from '@imgly/plugin-utils';
-import { OutputKind, Output } from '../core/provider';
-import { InitializationContext } from '../types';
+import { OutputKind } from '../core/provider';
+import CreativeEditorSDK from '@cesdk/cesdk-js';
 
 /**
  * Initializes the combined history asset source for the given asset sources.
  */
-function initializeHistoryCompositeAssetSource<
-  K extends OutputKind,
-  I,
-  O extends Output
->(
-  context: InitializationContext<K, I, O>,
-  historAssetSourceIds: string[]
-): string | undefined {
-  const {
-    options: { cesdk }
-  } = context;
+function initializeHistoryCompositeAssetSource<K extends OutputKind>(options: {
+  kind: K;
+  cesdk: CreativeEditorSDK;
+  historAssetSourceIds: string[];
+}): string | undefined {
+  const { kind, cesdk, historAssetSourceIds } = options;
+  const compositeAssetSourceId = `ly.img.ai.${kind}-generation.history`;
+
+  if (cesdk.engine.asset.findAllSources().includes(compositeAssetSourceId)) {
+    return compositeAssetSourceId;
+  }
 
   const aggregatedImageAssetSource = new AggregatedAssetSource(
-    'ly.img.ai.image-generation.history',
+    compositeAssetSourceId,
     cesdk,
     historAssetSourceIds
   );
